e44b465410e800b9475b04f95e9fbe8884e83496
Marc Legros <auflo2007@gmail.com>
2026-09-07 10:58:39 +0200

    :zap: feat: implement like button functionality and local storage for liked songs

--- public/script.js
+++ public/script.js
@@ -16,3 +16,21 @@ async function fetchCurrentTrack() {
 
 setInterval(fetchCurrentTrack, 5000);
 fetchCurrentTrack();
+
+
+const likeBtn = document.getElementById('likeBtn')
+
+// Récupère la liste des likes déjà sauvegardés (ou tableau vide si rien)
+function getLikes() {
+    const datalike = localStorage.getItem('likedSongs');
+    if (data) {
+        return JSON.parse(data);
+    } else {
+        return [];
+    }
+}
+
+// Sauvegarde la liste des likes
+function saveLikes(likes) {
+    localStorage.setItem('likedSongs', JSON.stringify(likes));
+}
